6bde12e9d0399101ca86cd88b53bfc1017ebab95
rutul7802 <67472683+rutul7802@users.noreply.github.com>
2020-07-02 13:41:03 +0530

    get_masks-utils-pysots-dataset

--- __init__.py
+++ __init__.py
@@ -1,15 +1,29 @@
+# --------------------------------------------------------
+# Python Single Object Tracking Evaluation
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Fangyi Zhang
+# @author fangyi.zhang@vipl.ict.ac.cn
+# @project https://github.com/StrangerZhang/pysot-toolkit.git
+# Revised for SiamMask by foolwood
+# --------------------------------------------------------
+from .vot import VOTDataset
 
-from torch.utils.ffi import _wrap_function
-from ._correlation import lib as _lib, ffi as _ffi
 
-__all__ = []
-def _import_symbols(locals):
-    for symbol in dir(_lib):
-        fn = getattr(_lib, symbol)
-        if callable(fn):
-            locals[symbol] = _wrap_function(fn, _ffi)
+class DatasetFactory(object):
+    @staticmethod
+    def create_dataset(**kwargs):
+        """
+        Args:
+            name: dataset name 'VOT2018', 'VOT2016'
+            dataset_root: dataset root
+        Return:
+            dataset
+        """
+        assert 'name' in kwargs, "should provide dataset name"
+        name = kwargs['name']
+        if 'VOT2018' == name or 'VOT2016' == name:
+            dataset = VOTDataset(**kwargs)
         else:
-            locals[symbol] = fn
-        __all__.append(symbol)
+            raise Exception("unknow dataset {}".format(kwargs['name']))
+        return dataset
 
-_import_symbols(locals())
