4097509767ae85b1f6a2dc613bf17c821a2f9cab
rutul7802 <67472683+rutul7802@users.noreply.github.com>
2020-07-02 12:10:35 +0530

    inpainting- channelnorm- packages - modules

--- channelnorm.py
+++ channelnorm.py
@@ -1,35 +1,14 @@
-import torch
-from torch.autograd import Function
-from .._ext import channelnorm
+from torch.nn.modules.module import Module
 
+from ..functions.channelnorm import ChannelNormFunction
 
-class ChannelNormFunction(Function):
-
+class ChannelNorm(Module):
     def __init__(self, norm_deg=2):
-        super(ChannelNormFunction, self).__init__()
+        super(ChannelNorm, self).__init__()
         self.norm_deg = norm_deg
 
     def forward(self, input1):
-        # self.save_for_backward(input1)
 
-        assert(input1.is_contiguous() == True)
+        result = ChannelNormFunction(self.norm_deg)(input1)
 
-        with torch.cuda.device_of(input1):
-            b, _, h, w = input1.size()
-            output = input1.new().resize_(b, 1, h, w).zero_()
-
-            channelnorm.ChannelNorm_cuda_forward(input1, output, self.norm_deg)
-        self.save_for_backward(input1, output)
-
-        return output
-
-    def backward(self, gradOutput):
-        input1, output = self.saved_tensors
-
-        with torch.cuda.device_of(input1):
-            b, c, h, w = input1.size()
-            gradInput1 = input1.new().resize_(b,c,h,w).zero_()
-
-            channelnorm.ChannelNorm_cuda_backward(input1, output, gradOutput, gradInput1, self.norm_deg)
-
-        return gradInput1
+        return result
