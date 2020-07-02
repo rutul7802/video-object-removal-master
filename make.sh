04e049f34209ac8fb67afa7a3c3c94225144a5c1
rutul7802 <67472683+rutul7802@users.noreply.github.com>
2020-07-02 13:32:26 +0530

    get_masks

--- make.sh
+++ make.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-TORCH=$(python -c "import os; import torch; print(os.path.dirname(torch.__file__))")
 
-cd src
+cd utils/pyvotkit
+python setup.py build_ext --inplace
+cd ../../
 
-echo "Compiling correlation kernels by nvcc..."
-
-rm correlation_cuda_kernel.o
-rm -r ../_ext
-
-nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
-
-cd ../
-python build.py
+cd utils/pysot/utils/
+python setup.py build_ext --inplace
+cd ../../../..
